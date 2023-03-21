7b43922580ad51dee7decec48dfd397edab914ef
amariths <amar.mehdi@iths.se>
2023-03-21 12:33:14 +0100

    dsfads

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import vue from "@vitejs/plugin-vue";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/vue-grupp-project",
+  base: "/test-page",
   plugins: [vue()],
   resolve: {
     alias: {
